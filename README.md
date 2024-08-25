139a296ce1d623387d5bd9ca03e18fce7f69193e
Ilya Chehov <ichehoc96@gmail.com>
2024-08-25 19:18:45 +0400

    Add functionality for rendering and deleting cards

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+Ссылка на репозиторий:  https://github.com/IlyaChehov/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function renderCard (elementCard, deleteCard) {
+  const itemCard = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  itemCard.querySelector('.card__image').src = elementCard.link;
+  itemCard.querySelector('.card__image').alt = elementCard.name;
+  itemCard.querySelector('.card__title').textContent = elementCard.name;
+  itemCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return itemCard;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCard () {
+  event.target.closest('.places__item').remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (item) {
+  cardContainer.append(renderCard(item, deleteCard));
+});

ff824d5ed469576fdee341c971281ef8392a77b3
Ilya Chehov <ichehoc96@gmail.com>
2024-10-06 02:18:07 +0400

    add validation input

--- src/index.js
+++ src/index.js
@@ -102,3 +102,64 @@ popups.forEach((popup) => {
 });
 formEditProfile.addEventListener('submit', editProfile);
 formNewPlace.addEventListener('submit', addNewCard);
+
+/////////////////////////////////
+
+const validationData = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+enableValidation (validationData);
+
+function enableValidation (validationData) {
+  const formList = Array.from(document.querySelectorAll(validationData.formSelector));
+
+  formList.forEach((formElement) => {
+    setEventListener (formElement);
+  });
+};
+
+function setEventListener (formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(validationData.inputSelector));
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid (formElement, inputElement);
+    })
+  });
+};
+
+function isValid (formElement, inputElement) {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.error);
+  } else {
+    inputElement.setCustomValidity('');
+  };
+
+  if (!inputElement.validity.valid) {
+    showInputError (formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hiddenInputError (formElement, inputElement);
+  };
+};
+
+function showInputError (formElement, inputElement, errorMessage) {
+ const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+ inputElement.classList.add(validationData.inputErrorClass);
+ errorElement.textContent = errorMessage;
+ errorElement.classList.add(validationData.errorClass);
+};
+
+function hiddenInputError (formElement, inputElement) {
+ const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+ inputElement.classList.remove(validationData.inputErrorClass);
+ errorElement.classList.remove(validationData.errorClass);
+ errorElement.textContent = '';
+};
